9dcae31e4104b10dc8b99fef93c79ca42e017f8c
frankkwabenaaboagye <frank.aboagye@amalitech.com>
2025-02-11 10:19:23 +0000

    add profiles

--- spring-security/jwt/jwtAuthenAndAthor/socialmedia/src/main/java/frank/app/socialmedia/controller/SocialMediaController.java
+++ spring-security/jwt/jwtAuthenAndAthor/socialmedia/src/main/java/frank/app/socialmedia/controller/SocialMediaController.java
@@ -3,6 +3,8 @@ package frank.app.socialmedia.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,5 +17,10 @@ public class SocialMediaController {
     @GetMapping("/homepage")
     public ResponseEntity<String> homepage(){
         return ResponseEntity.ok("Welcome to Social Media App");
+    }
+
+    @GetMapping("/profile")
+    public ResponseEntity<String> profile(@AuthenticationPrincipal UserDetails userDetails){
+        return ResponseEntity.ok("Welcome to your profile " + userDetails.getUsername());
     }
 }
