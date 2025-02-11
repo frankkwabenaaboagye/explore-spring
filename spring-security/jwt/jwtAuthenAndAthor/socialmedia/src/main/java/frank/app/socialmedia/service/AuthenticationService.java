831e1147d70cae6b406ec28da5396a29fa578878
frankkwabenaaboagye <frank.aboagye@amalitech.com>
2025-02-11 08:49:41 +0000

    add implementation to authenticate users

--- spring-security/jwt/jwtAuthenAndAthor/socialmedia/src/main/java/frank/app/socialmedia/service/AuthenticationService.java
+++ spring-security/jwt/jwtAuthenAndAthor/socialmedia/src/main/java/frank/app/socialmedia/service/AuthenticationService.java
@@ -1,6 +1,5 @@
 package frank.app.socialmedia.service;
 
-import frank.app.socialmedia.appUser.Role;
 import frank.app.socialmedia.appUser.SocialMediaUser;
 import frank.app.socialmedia.config.JwtService;
 import frank.app.socialmedia.repository.SocialMediaUserRepository;
@@ -8,11 +7,12 @@ import frank.app.socialmedia.utils.AuthenticationRequest;
 import frank.app.socialmedia.utils.AuthenticationResponse;
 import frank.app.socialmedia.utils.RegistrationRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.User;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import static frank.app.socialmedia.appUser.Role.*;
+import static frank.app.socialmedia.appUser.Role.NORMALUSER;
 
 @Service
 @RequiredArgsConstructor
@@ -21,6 +21,7 @@ public class AuthenticationService {
     private final SocialMediaUserRepository socialMediaUserRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
 
     public AuthenticationResponse register(RegistrationRequest registrationRequest) {
 
@@ -41,6 +42,18 @@ public class AuthenticationService {
     }
 
     public AuthenticationResponse authenticate(AuthenticationRequest authenticationRequest) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        authenticationRequest.getEmail(),
+                        authenticationRequest.getPassword()
+                )
+        );
+
+        SocialMediaUser user = socialMediaUserRepository.findByEmail(authenticationRequest.getEmail()).orElseThrow();
+        var jwtToken = jwtService.generateJwt(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
 
     }
 
