681938aa461486cd473b6b1a96c903f92906af66
frankkwabenaaboagye <frank.aboagye@amalitech.com>
2025-04-13 17:12:06 +0000

    movie catalog service

--- microservice/movie-catalog-api/movie-catalog-service/src/main/java/org/frank/moviecatalogservice/model/CatalogItem.java
+++ microservice/movie-catalog-api/movie-catalog-service/src/main/java/org/frank/moviecatalogservice/model/CatalogItem.java
@@ -1,4 +1,15 @@
 package org.frank.moviecatalogservice.model;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class CatalogItem {
+
+    private String name;
+    private String desc;
+    private int rating;
+
+
 }
